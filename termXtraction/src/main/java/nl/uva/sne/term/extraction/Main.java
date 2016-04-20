df44a3d044f2b9b6742c396ca9429c3909c909c0
skoulouzis <S.Koulouzis@uva.nl>
2016-04-20 19:52:30 +0200

    rename

--- termXtraction/src/main/java/nl/uva/sne/term/extraction/Main.java
+++ termXtraction/src/main/java/nl/uva/sne/term/extraction/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nl.uva.sne.termExtraction;
+package nl.uva.sne.term.extraction;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
