0160139f9a23b82655cc0c72ffe8bd63c892bb8c
skoulouzis <S.Koulouzis@uva.nl>
2016-04-20 20:17:02 +0200

    props file

--- termXtraction/src/main/java/nl/uva/sne/term/extraction/Main.java
+++ termXtraction/src/main/java/nl/uva/sne/term/extraction/Main.java
@@ -112,6 +112,7 @@ public class Main {
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
             in = classLoader.getResourceAsStream(propertiesPath);
             Properties properties = new Properties();
+             System.err.println(propertiesPath);
             System.err.println(in);
             properties.load(in);
             return properties;
