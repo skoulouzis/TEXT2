e142b8c983c646ad3b8443ac87e99fb37c71533a
skoulouzis <S.Koulouzis@uva.nl>
2016-06-16 15:54:16 +0200

    grep terms from dictionaryAll

--- scripts/disambiguateTraningTerms.sh
+++ scripts/disambiguateTraningTerms.sh
@@ -4,7 +4,11 @@ JAR_PATH=$HOME/workspace/TEXT2/termDisambiguation/target/termDisambiguation-1.0-
 PROPS_FILE=$HOME/workspace/TEXT2/scripts/props.properties
 DICTIONARY_ALL=$HOME/Downloads/textdocs/dictionaryAll.csv
 
-declare -a TRAIN_DOC_PATHS=("$HOME/Downloads/doc3/1Data_Analytics_and_Machine_Learning/" "$HOME/Downloads/doc3/2Data_Management_data_Curation/" "$HOME/Downloads/doc3/3Data_Science_Engineering/" "$HOME/Downloads/doc3/4Scientific_Research_Methods/" "$HOME/Downloads/doc3/5PersonalInter-personalCommunication" "$HOME/Downloads/doc3/2Data_Management_data_Curation/" "$HOME/Downloads/doc3/6ApplicationSubjectDomain")
+# declare -a TRAIN_DOC_PATHS=("$HOME/Downloads/doc3/1Data_Analytics_and_Machine_Learning/" "$HOME/Downloads/doc3/2Data_Management_data_Curation/" "$HOME/Downloads/doc3/3Data_Science_Engineering/" "$HOME/Downloads/doc3/4Scientific_Research_Methods/" "$HOME/Downloads/doc3/5PersonalInter-personalCommunication" "$HOME/Downloads/doc3/2Data_Management_data_Curation/" "$HOME/Downloads/doc3/6ApplicationSubjectDomain")
+
+
+declare -a TRAIN_DOC_PATHS=("$HOME/Downloads/D2.2_Table_14CompetencesGroups/DataAnalytics_DSDA/" "$HOME/Downloads/D2.2_Table_14CompetencesGroups/DataManagementCuration_DSDM/" "$HOME/Downloads/D2.2_Table_14CompetencesGroups/DSDomain_DSDK/" "$HOME/Downloads/D2.2_Table_14CompetencesGroups/DSEngineering_DSENG/" "$HOME/Downloads/D2.2_Table_14CompetencesGroups/ScientificResearchMethods_DSRM")
+
 
 for i in "${TRAIN_DOC_PATHS[@]}"
 do
