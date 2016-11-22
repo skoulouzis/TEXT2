e74df61f338cf1ec0cd481ef8e03ee3ebe2fc229
skoulouzis <S.Koulouzis@uva.nl>
2016-11-22 17:17:02 +0100

    Changed conf files

--- termXtraction/src/main/java/nl/uva/sne/extractors/JtopiaExtractor.java
+++ termXtraction/src/main/java/nl/uva/sne/extractors/JtopiaExtractor.java
@@ -26,95 +26,95 @@ import org.apache.commons.io.FilenameUtils;
  */
 public class JtopiaExtractor implements TermExtractor {
 
-    @Override
-    public void configure(Properties prop) {
-        for (Object k : prop.keySet()) {
-            Logger.getLogger(JtopiaExtractor.class.getName()).log(Level.INFO, k + " : " + prop.getProperty((String) k));
-        }
-        String taggerType = prop.getProperty("tagger.type", "stanford");
-
-        String modelPath = prop.getProperty("model.path",
-                System.getProperty("user.home") + File.separator + "workspace"
-                + File.separator + "TEXT2" + File.separator + "etc" + File.separator + "model");
-        if (modelPath.endsWith("/")) {
-            modelPath = modelPath.substring(0, modelPath.length() - 1);
-        }
-        switch (taggerType) {
-            case "stanford":
-                Configuration.setModelFileLocation(modelPath + File.separator
-                        + "stanford" + File.separator + "english-left3words-distsim.tagger");
-                Configuration.setTaggerType("stanford");
-                break;
-            case "openNLP":
-                Configuration.setModelFileLocation(modelPath + File.separator
-                        + "openNLP" + File.separator + "en-pos-maxent.bin");
-                Configuration.setTaggerType("openNLP");
-                break;
-            case "default":
-                Configuration.setModelFileLocation(modelPath + File.separator
-                        + "default" + File.separator + "english-lexicon.txt");
-                Configuration.setTaggerType("default");
-                break;
-        }
-        Integer singleStrength = Integer.valueOf(prop.getProperty("single.strength", "3"));
-        Configuration.setSingleStrength(singleStrength);
-
-        Integer noLimitStrength = Integer.valueOf(prop.getProperty("no.limit.strength", "2"));
-        Configuration.setNoLimitStrength(noLimitStrength);
+  @Override
+  public void configure(Properties prop) {
+    for (Object k : prop.keySet()) {
+      Logger.getLogger(JtopiaExtractor.class.getName()).log(Level.INFO, "{0} : {1}", new Object[]{k, prop.getProperty((String) k)});
     }
+    String taggerType = prop.getProperty("tagger.type", "stanford");
 
-    @Override
-    public Map<String, Double> termXtraction(String inDir) throws IOException {
-        File dir = new File(inDir);
-        TermsExtractor termExtractor = new TermsExtractor();
-        TermDocument topiaDoc = new TermDocument();
-        HashMap<String, Double> keywordsDictionaray = new HashMap();
-        int count = 0;
-        if (dir.isDirectory()) {
-            for (File f : dir.listFiles()) {
-                if (FilenameUtils.getExtension(f.getName()).endsWith("txt")) {
-                    count++;
-                    Logger.getLogger(JtopiaExtractor.class.getName()).log(Level.INFO, "{0}: {1} of {2}", new Object[]{f.getName(), count, dir.list().length});
-                    keywordsDictionaray.putAll(extractFromFile(f, termExtractor, topiaDoc));
-                }
-            }
-        } else if (dir.isFile()) {
-            keywordsDictionaray.putAll(extractFromFile(dir, termExtractor, topiaDoc));
-        }
-        return keywordsDictionaray;
+    String modelPath = prop.getProperty("model.path",
+            System.getProperty("user.home") + File.separator + "workspace"
+            + File.separator + "TEXT2" + File.separator + "etc" + File.separator + "model");
+    if (modelPath.endsWith("/")) {
+      modelPath = modelPath.substring(0, modelPath.length() - 1);
     }
+    switch (taggerType) {
+      case "stanford":
+        Configuration.setModelFileLocation(modelPath + File.separator
+                + "stanford" + File.separator + "english-left3words-distsim.tagger");
+        Configuration.setTaggerType("stanford");
+        break;
+      case "openNLP":
+        Configuration.setModelFileLocation(modelPath + File.separator
+                + "openNLP" + File.separator + "en-pos-maxent.bin");
+        Configuration.setTaggerType("openNLP");
+        break;
+      case "default":
+        Configuration.setModelFileLocation(modelPath + File.separator
+                + "default" + File.separator + "english-lexicon.txt");
+        Configuration.setTaggerType("default");
+        break;
+    }
+    Integer singleStrength = Integer.valueOf(prop.getProperty("single.strength", "3"));
+    Configuration.setSingleStrength(singleStrength);
 
-    private Map<String, Double> extractFromFile(File f, TermsExtractor termExtractor, TermDocument topiaDoc) throws IOException {
-        HashMap<String, Double> keywordsDictionaray = null;
+    Integer noLimitStrength = Integer.valueOf(prop.getProperty("no.limit.strength", "2"));
+    Configuration.setNoLimitStrength(noLimitStrength);
+  }
+
+  @Override
+  public Map<String, Double> termXtraction(String inDir) throws IOException {
+    File dir = new File(inDir);
+    TermsExtractor termExtractor = new TermsExtractor();
+    TermDocument topiaDoc = new TermDocument();
+    HashMap<String, Double> keywordsDictionaray = new HashMap();
+    int count = 0;
+    if (dir.isDirectory()) {
+      for (File f : dir.listFiles()) {
         if (FilenameUtils.getExtension(f.getName()).endsWith("txt")) {
-            keywordsDictionaray = new HashMap();
-            try (BufferedReader br = new BufferedReader(new FileReader(f))) {
-                StringBuilder stringBuffer = new StringBuilder();
-                for (String text; (text = br.readLine()) != null;) {
-                    text = text.replaceAll("-", "");
-                    text = text.replaceAll("((mailto\\:|(news|(ht|f)tp(s?))\\://){1}\\S+)", "");
-                    text = text.replaceAll("[^a-zA-Z\\s]", "");
-//        text = text.replaceAll("(\\d+,\\d+)|\\d+", "");
-                    text = text.replaceAll("  ", " ");
-                    text = text.toLowerCase();
-                    stringBuffer.append(text).append("\n");
-                }
-
-                topiaDoc = termExtractor.extractTerms(stringBuffer.toString());
-                Set<String> terms = topiaDoc.getFinalFilteredTerms().keySet();
-                for (String t : terms) {
-                    String text = t.replaceAll(" ", "_");
-                    Double tf;
-                    if (keywordsDictionaray.containsKey(text.toLowerCase())) {
-                        tf = keywordsDictionaray.get(text.toLowerCase());
-                        tf++;
-                    } else {
-                        tf = 1.0;
-                    }
-                    keywordsDictionaray.put(text.toLowerCase(), tf);
-                }
-            }
+          count++;
+          Logger.getLogger(JtopiaExtractor.class.getName()).log(Level.INFO, "{0}: {1} of {2}", new Object[]{f.getName(), count, dir.list().length});
+          keywordsDictionaray.putAll(extractFromFile(f, termExtractor, topiaDoc));
         }
-        return keywordsDictionaray;
+      }
+    } else if (dir.isFile()) {
+      keywordsDictionaray.putAll(extractFromFile(dir, termExtractor, topiaDoc));
     }
+    return keywordsDictionaray;
+  }
+
+  private Map<String, Double> extractFromFile(File f, TermsExtractor termExtractor, TermDocument topiaDoc) throws IOException {
+    HashMap<String, Double> keywordsDictionaray = null;
+    if (FilenameUtils.getExtension(f.getName()).endsWith("txt")) {
+      keywordsDictionaray = new HashMap();
+      try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+        StringBuilder stringBuffer = new StringBuilder();
+        for (String text; (text = br.readLine()) != null;) {
+          text = text.replaceAll("-", "");
+          text = text.replaceAll("((mailto\\:|(news|(ht|f)tp(s?))\\://){1}\\S+)", "");
+          text = text.replaceAll("[^a-zA-Z\\s]", "");
+//        text = text.replaceAll("(\\d+,\\d+)|\\d+", "");
+          text = text.replaceAll("  ", " ");
+          text = text.toLowerCase();
+          stringBuffer.append(text).append("\n");
+        }
+
+        topiaDoc = termExtractor.extractTerms(stringBuffer.toString());
+        Set<String> terms = topiaDoc.getFinalFilteredTerms().keySet();
+        for (String t : terms) {
+          String text = t.replaceAll(" ", "_");
+          Double tf;
+          if (keywordsDictionaray.containsKey(text.toLowerCase())) {
+            tf = keywordsDictionaray.get(text.toLowerCase());
+            tf++;
+          } else {
+            tf = 1.0;
+          }
+          keywordsDictionaray.put(text.toLowerCase(), tf);
+        }
+      }
+    }
+    return keywordsDictionaray;
+  }
 }
